4b0cf5e92f3c0393aceeae91950f8afae845659f
vivask <vivask@mail.ru>
2023-02-10 11:30:44 +0300

    initial commit

--- main/main.c
+++ main/main.c
@@ -56,9 +56,11 @@ static void pool_radsens() {
     //device_set_state(&_peripheral.radsens, ret);
     if(ret == ESP_ERR_TIMEOUT){
         //FLASH_LOGD("RadSens I2C Timeout");
+        https_loge("RadSens I2C Timeout");
     } else if(ret == ESP_OK) {
         https_radsens_send_data(intensy_dyanmic, intensy_static, pulse);
     } else {
+        https_loge("%s: No ack, radsens not connected...skip...", esp_err_to_name(ret));
         //FLASH_LOGE("%s: No ack, radsens not connected...skip...", esp_err_to_name(ret));    
     }
 }
