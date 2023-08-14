eabc461b9e7a263b6d06f54a113d1974c8e86ee0
vivask <vivask@mail.ru>
2023-08-14 07:28:13 +0300

    initial commit

--- components/wifi-manager/component.mk
+++
@@ -1,4 +0,0 @@
-COMPONENT_ADD_INCLUDEDIRS = src
-COMPONENT_SRCDIRS = src
-COMPONENT_DEPENDS = log esp_http_server
-COMPONENT_EMBED_FILES := src/code.js src/style.css src/index.html
--- components/wifi-manager/ui/timezones.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat ./timezones.csv | awk -F\" '{print "<option value=\""$4"\">"$2"</option>"}' > ./timezones.html
